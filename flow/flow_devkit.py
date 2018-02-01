67d3427de125359177f7046d3ae7731ac5f176e4
hci-benchmark <hci-benchmark@iwr.uni-heidelberg.de>
2018-02-01 02:51:08 +0100

    Added HD1K benchmark

--- flow/flow_devkit.py
+++ flow/flow_devkit.py
@@ -6,7 +6,7 @@ from benchmark import *
 from benchmark_sintel import *
 from benchmark_kitti2015 import *
 from benchmark_middlebury import *
-# from benchmark_hci import *
+from benchmark_hd1k import *
 from dataset_format import *
 from dataset_format_middlebury import *
 from dataset_format_kitti2015 import *
@@ -16,8 +16,7 @@ from devkit import *
 if __name__ == '__main__':
     # Define the list of benchmarks supported by this script (in the order in
     # which they are listed on http://www.robustvision.net/index.php).
-    # benchmarks = [Kitti2015(), Sintel()]
-    benchmarks = [Middlebury(),]
+    benchmarks = [Kitti2015(), Sintel(), Middlebury(), HD1K2018()]
     
     # Define the list of dataset formats which are supported.
     dataset_formats = [MiddleburyFormat(), Kitti2015Format()]
