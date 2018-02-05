760af3cf9fc1890c1b335e071ca6c4384b01bf49
Jonas Uhrig <uhrig.jonas@gmail.com>
2018-02-05 20:31:32 +0100

    updated downsampling of ScanNet RGB images (to match depth map resolution)

--- depth/ScanNet/downsample_color_imgs.py
+++ depth/ScanNet/downsample_color_imgs.py
@@ -3,13 +3,14 @@ import glob
 import os
 import sys
 
-color_img_files = sorted(glob.glob("datasets_ScanNet/scene*_*/color/*.png"))
-depth_img_files = sorted(glob.glob("datasets_ScanNet/scene*_*/depth/*.png"))
+w_depth = 640
+h_depth = 480
 
-for img_fn, depth_fn in zip(color_img_files, depth_img_files):
+color_img_files = sorted(glob.glob("datasets_ScanNet/scene*_*/color/*.png"))
+
+for img_fn in color_img_files:
     print "\rProcessing file {}".format(img_fn),
     sys.stdout.flush()
-    w_depth, h_depth = Image.open(depth_fn).size
     new_fn = img_fn.replace("color", "color_rob")
     if not os.path.isdir(os.path.dirname(new_fn)):
         os.makedirs(os.path.dirname(new_fn))
