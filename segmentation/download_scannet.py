d11e059be66e6f039f5e83b630ca136facf024ef
angeladai <adai@cs.stanford.edu>
2018-04-06 05:16:31 -0700

    small scannet script change for python3

--- segmentation/download_scannet.py
+++ segmentation/download_scannet.py
@@ -35,7 +35,7 @@ def download_release(release_scans, out_dir, file_types):
 def download_file(url, out_file):
     out_dir = os.path.dirname(out_file)
     if not os.path.isfile(out_file):
-        print '\t' + url + ' > ' + out_file
+        print('\t' + url + ' > ' + out_file)
         fh, out_file_tmp = tempfile.mkstemp(dir=out_dir)
         f = os.fdopen(fh, 'w')
         f.close()
