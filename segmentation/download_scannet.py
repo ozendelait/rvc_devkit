ed50a1837cf8a08e36523ef6e7bbdac8147acdea
Oliver Zendel <ozendelait@gmail.com>
2020-04-15 18:50:38 +0200

    fixing broken scannet link

--- segmentation/download_scannet.py
+++ segmentation/download_scannet.py
@@ -7,7 +7,7 @@ import os
 import urllib
 import tempfile
 
-BASE_URL = 'http://dovahkiin.stanford.edu/scannet-public/'
+BASE_URL = 'http://kaldir.vc.in.tum.de/scannet/'
 TOS_URL = BASE_URL + 'ScanNet_TOS.pdf'
 FILETYPES = ['.aggregation.json', '.sens', '.txt', '_vh_clean.ply', '_vh_clean_2.0.010000.segs.json', '_vh_clean_2.ply', '_vh_clean.segs.json', '_vh_clean.aggregation.json', '_vh_clean_2.labels.ply', '_2d-instance.zip', '_2d-instance-filt.zip', '_2d-label.zip', '_2d-label-filt.zip']
 RELEASE = 'v1/scans'
