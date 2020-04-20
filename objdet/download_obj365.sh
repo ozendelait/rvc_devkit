0775be07dd3859c1b6b9c03e33e14fc7fa358449
Oliver Zendel <oliver.zendel@ait.ac.at>
2020-04-20 11:57:18 +0200

    -added extraction file

--- objdet/download_obj365.sh
+++ objdet/download_obj365.sh
@@ -28,6 +28,13 @@ echo "Downloading objects365 to ${RVC_OBJ365_TRG_DIR}"
 mkdir -p ${RVC_OBJ365_TRG_DIR}
 aws s3 --no-sign-request sync s3://objects365/objects365-v2-released/ ${RVC_OBJ365_TRG_DIR}
 
+for onetarfile in ${RVC_OBJ365_TRG_DIR}/*.tar.gz
+do
+  echo "Extracting tar file $onetarfile..."
+  tar zxf $onetarfile -C ${RVC_OBJ365_TRG_DIR}/../
+  rm $onetarfile
+done
+
 RVC_OBJ365_TRG_DIR=
 RVC_OBJ365_SCRIPT_DIR=
 
