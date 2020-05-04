b3a9f71b50cec21137c6aa4068f41a4d2b2ae71d
Oliver Zendel <oliver.zendel@ait.ac.at>
2020-05-04 12:01:55 +0200

    -updating download script to include masks

--- segmentation/download_oid.sh
+++ segmentation/download_oid.sh
@@ -19,24 +19,21 @@ echo "WARNING: the size of the full dataset is around 500GB, make sure you have 
 mkdir -p ${RVC_OID_TRG_DIR}
 cd ${RVC_OID_TRG_DIR}
 
+hexendings=0123456789abcdef
+for (( i=0; i<${#hexendings}; i++ )); do
+  #aws s3 --no-sign-request cp s3://open-images-dataset/tar/train_${hexendings:$i:1}.tar.gz ${RVC_OID_TRG_DIR}
+  wget --no-check-certificate  https://storage.googleapis.com/openimages/challenge_2019/train-masks/challenge-2019-train-masks-${hexendings:$i:1}.zip -P ${RVC_OID_TRG_DIR}/challenge_2019_train_masks/
+  wget --no-check-certificate https://storage.googleapis.com/openimages/challenge_2019/validation-masks/challenge-2019-validation-masks-${hexendings:$i:1}.zip -P ${RVC_OID_TRG_DIR}/challenge_2019_train_masks/
+done
 
-aws s3 --no-sign-request cp s3://open-images-dataset/tar/train_0.tar.gz ${RVC_OID_TRG_DIR}
-aws s3 --no-sign-request cp s3://open-images-dataset/tar/train_1.tar.gz ${RVC_OID_TRG_DIR}
-aws s3 --no-sign-request cp s3://open-images-dataset/tar/train_2.tar.gz ${RVC_OID_TRG_DIR}
-aws s3 --no-sign-request cp s3://open-images-dataset/tar/train_3.tar.gz ${RVC_OID_TRG_DIR}
-aws s3 --no-sign-request cp s3://open-images-dataset/tar/train_4.tar.gz ${RVC_OID_TRG_DIR}
-aws s3 --no-sign-request cp s3://open-images-dataset/tar/train_5.tar.gz ${RVC_OID_TRG_DIR}
-aws s3 --no-sign-request cp s3://open-images-dataset/tar/train_6.tar.gz ${RVC_OID_TRG_DIR}
-aws s3 --no-sign-request cp s3://open-images-dataset/tar/train_7.tar.gz ${RVC_OID_TRG_DIR}
-aws s3 --no-sign-request cp s3://open-images-dataset/tar/train_8.tar.gz ${RVC_OID_TRG_DIR}
-aws s3 --no-sign-request cp s3://open-images-dataset/tar/train_9.tar.gz ${RVC_OID_TRG_DIR}
-aws s3 --no-sign-request cp s3://open-images-dataset/tar/train_a.tar.gz ${RVC_OID_TRG_DIR}
-aws s3 --no-sign-request cp s3://open-images-dataset/tar/train_b.tar.gz ${RVC_OID_TRG_DIR}
-aws s3 --no-sign-request cp s3://open-images-dataset/tar/train_c.tar.gz ${RVC_OID_TRG_DIR}
-aws s3 --no-sign-request cp s3://open-images-dataset/tar/train_d.tar.gz ${RVC_OID_TRG_DIR}
-aws s3 --no-sign-request cp s3://open-images-dataset/tar/train_e.tar.gz ${RVC_OID_TRG_DIR}
-aws s3 --no-sign-request cp s3://open-images-dataset/tar/train_f.tar.gz ${RVC_OID_TRG_DIR}
-aws s3 --no-sign-request cp s3://open-images-dataset/tar/validation.tar.gz ${RVC_OID_TRG_DIR}
+
+wget --no-check-certificate https://storage.googleapis.com/openimages/challenge_2019/challenge-2019-train-detection-human-imagelabels.csv -P ${RVC_OID_TRG_DIR}/annotations
+wget --no-check-certificate https://storage.googleapis.com/openimages/challenge_2019/challenge-2019-validation-detection-human-imagelabels.csv -P ${RVC_OID_TRG_DIR}/annotations
+wget --no-check-certificate https://storage.googleapis.com/openimages/2018_04/train/train-images-boxable-with-rotation.csv -P ${RVC_OID_TRG_DIR}/annotations
+wget --no-check-certificate https://storage.googleapis.com/openimages/2018_04/validation/validation-images-with-rotation.csv -P ${RVC_OID_TRG_DIR}/annotations
+wget --no-check-certificate https://storage.googleapis.com/openimages/challenge_2019/challenge-2019-classes-description-500.csv -P ${RVC_OID_TRG_DIR}/annotations
+wget --no-check-certificate https://storage.googleapis.com/openimages/challenge_2019/challenge-2019-train-segmentation-masks.csv -P ${RVC_OID_TRG_DIR}/annotations
+wget --no-check-certificate https://storage.googleapis.com/openimages/challenge_2019/challenge-2019-validation-segmentation-masks.csv -P ${RVC_OID_TRG_DIR}/annotations
 
 RVC_OID_TRG_DIR=
 RVC_OID_SCRIPT_DIR=
