2bc8660d1d6166dd1511ed68619d6fce6d46a785
Oliver Zendel <oliver.zendel@ait.ac.at>
2020-05-19 18:46:59 +0200

    -adding missing files for mseg usage

--- segmentation/remap_sem_segm.sh
+++ segmentation/remap_sem_segm.sh
@@ -17,17 +17,21 @@ if [ -z "${RVC_JOINED_TRG_DIR}" ]; then
 else
   RVC_DATA_TRG_DIR=${RVC_JOINED_TRG_DIR}/
 fi
-
-if [ ! -d $RVC_SEM_SEG_SCRIPT_DIR/mseg-api ]; then
-  # getting defined version of mseg repo
-  git -C $RVC_SEM_SEG_SCRIPT_DIR clone https://github.com/mseg-dataset/mseg-api.git 
-  git -C $RVC_SEM_SEG_SCRIPT_DIR/mseg-api checkout 29f40956abd227aa7d454f05cd8f007de0d12a09
-  #git -C $RVC_SEM_SEG_SCRIPT_DIR/openimages2coco apply $RVC_SEM_SEG_SCRIPT_DIR/openimages2coco.patch
-fi
-
 mkdir -p ${RVC_DATA_TRG_DIR}
 
+if [ ! -d $RVC_SEM_SEG_SCRIPT_DIR/mseg_api ]; then
+  # getting defined version of mseg repo
+  git -C $RVC_SEM_SEG_SCRIPT_DIR clone https://github.com/mseg-dataset/mseg-api.git $RVC_SEM_SEG_SCRIPT_DIR/mseg_api
+  git -C $RVC_SEM_SEG_SCRIPT_DIR/mseg_api checkout 29f40956abd227aa7d454f05cd8f007de0d12a09
+  git -C $RVC_SEM_SEG_SCRIPT_DIR/mseg_api apply $RVC_SEM_SEG_SCRIPT_DIR/mseg_api.patch
+fi
+
 pushd $RVC_SEM_SEG_SCRIPT_DIR/
+  if [ ! -d $RVC_SEM_SEG_SCRIPT_DIR/mseg_api/mseg/dataset_lists/rvc_uint8 ]; then
+    mkdir -p $RVC_SEM_SEG_SCRIPT_DIR/mseg_api/mseg/dataset_lists/rvc_uint8
+    cp ./rvc_uint8_names.txt $RVC_SEM_SEG_SCRIPT_DIR/mseg_api/mseg/dataset_lists/rvc_uint8
+  fi
+
   python ./mseg-api/mseg/label_preparation/remap_dataset.py --orig_dname ade20k-151 --remapped_dname rvc_uint8 --orig_dataroot ${RVC_DATA_SRC_DIR}/ade20k --remapped_dataroot ${RVC_DATA_TRG_DIR} --mapping_tsv ./ss_mapping_uint8_mseg.tsv
 popd
 
