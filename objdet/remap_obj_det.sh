44b42f4968becef84a8e463c7ea6a337b72f2504
Claudio <claudio.michaelis@uni-tuebingen.de>
2020-06-11 14:59:14 +0000

    Check if remapped files already exist before mapping them

--- objdet/remap_obj_det.sh
+++ objdet/remap_obj_det.sh
@@ -21,69 +21,84 @@ fi
 #convert oid gt
 $RVC_OBJ_DET_SCRIPT_DIR/convert_oid_coco.sh
 
+if [ ! -f "$RVC_DATA_TRG_DIR/coco_boxable.rvc_val.json" ]; then
+    python3 $RVC_OBJ_DET_SCRIPT_DIR/../common/remap_coco.py --input $RVC_DATA_SRC_DIR/coco/annotations/instances_val2017.json \
+                            --mapping $RVC_OBJ_DET_SCRIPT_DIR/obj_det_mapping.csv \
+                            --mapping_row coco_boxable_name \
+                            --image_root_rel $RVC_DATA_SRC_DIR/coco/images \
+                            --void_id 0 \
+                            --reduce_boxable \
+                            --output $RVC_DATA_TRG_DIR/coco_boxable.rvc_val.json
+    fi
 
-python3 $RVC_OBJ_DET_SCRIPT_DIR/../common/remap_coco.py --input $RVC_DATA_SRC_DIR/coco/annotations/instances_val2017.json \
-                        --mapping $RVC_OBJ_DET_SCRIPT_DIR/obj_det_mapping.csv \
-                        --mapping_row coco_boxable_name \
-                        --image_root_rel $RVC_DATA_SRC_DIR/coco/images \
-                        --void_id 0 \
-                        --reduce_boxable \
-                        --output $RVC_DATA_TRG_DIR/coco_boxable.rvc_val.json
+if [ ! -f "$RVC_DATA_TRG_DIR/coco_boxable.rvc_train.json" ]; then
+    python3 $RVC_OBJ_DET_SCRIPT_DIR/../common/remap_coco.py --input $RVC_DATA_SRC_DIR/coco/annotations/instances_train2017.json \
+                            --mapping $RVC_OBJ_DET_SCRIPT_DIR/obj_det_mapping.csv \
+                            --mapping_row coco_boxable_name \
+                            --image_root_rel $RVC_DATA_SRC_DIR/coco/images \
+                            --void_id 0 \
+                            --reduce_boxable \
+                            --output $RVC_DATA_TRG_DIR/coco_boxable.rvc_train.json
+fi
 
-python3 $RVC_OBJ_DET_SCRIPT_DIR/../common/remap_coco.py --input $RVC_DATA_SRC_DIR/coco/annotations/instances_train2017.json \
-                        --mapping $RVC_OBJ_DET_SCRIPT_DIR/obj_det_mapping.csv \
-                        --mapping_row coco_boxable_name \
-                        --image_root_rel $RVC_DATA_SRC_DIR/coco/images \
-                        --void_id 0 \
-                        --reduce_boxable \
-                        --output $RVC_DATA_TRG_DIR/coco_boxable.rvc_train.json
+if [ ! -f "$RVC_DATA_TRG_DIR/obj365_boxable.rvc_val.json" ]; then
+    python3 $RVC_OBJ_DET_SCRIPT_DIR/../common/remap_coco.py --input $RVC_DATA_SRC_DIR/objects365/objects365v2_val_0422.json \
+                            --mapping $RVC_OBJ_DET_SCRIPT_DIR/obj_det_mapping.csv \
+                            --mapping_row obj365_boxable_name \
+                            --image_root_rel $RVC_DATA_SRC_DIR/objects365/ \
+                            --reduce_boxable \
+                            --output $RVC_DATA_TRG_DIR/obj365_boxable.rvc_val.json
+fi
 
-python3 $RVC_OBJ_DET_SCRIPT_DIR/../common/remap_coco.py --input $RVC_DATA_SRC_DIR/objects365/objects365v2_val_0422.json \
-                        --mapping $RVC_OBJ_DET_SCRIPT_DIR/obj_det_mapping.csv \
-                        --mapping_row obj365_boxable_name \
-                        --image_root_rel $RVC_DATA_SRC_DIR/objects365/ \
-                        --reduce_boxable \
-                        --output $RVC_DATA_TRG_DIR/obj365_boxable.rvc_val.json
+if [ ! -f "$RVC_DATA_TRG_DIR/obj365_boxable.rvc_train.json" ]; then
+    python3 $RVC_OBJ_DET_SCRIPT_DIR/../common/remap_coco.py --input $RVC_DATA_SRC_DIR/objects365/objects365v2_train_0422.json \
+                            --mapping $RVC_OBJ_DET_SCRIPT_DIR/obj_det_mapping.csv \
+                            --mapping_row obj365_boxable_name \
+                            --image_root_rel $RVC_DATA_SRC_DIR/objects365/ \
+                            --void_id 0 \
+                            --reduce_boxable \
+                            --output $RVC_DATA_TRG_DIR/obj365_boxable.rvc_train.json
+fi
 
-python3 $RVC_OBJ_DET_SCRIPT_DIR/../common/remap_coco.py --input $RVC_DATA_SRC_DIR/objects365/objects365v2_train_0422.json \
-                        --mapping $RVC_OBJ_DET_SCRIPT_DIR/obj_det_mapping.csv \
-                        --mapping_row obj365_boxable_name \
-                        --image_root_rel $RVC_DATA_SRC_DIR/objects365/ \
-                        --void_id 0 \
-                        --reduce_boxable \
-                        --output $RVC_DATA_TRG_DIR/obj365_boxable.rvc_train.json
+if [ ! -f "$RVC_DATA_TRG_DIR/oid_boxable.rvc_val.json" ]; then
+    python3 $RVC_OBJ_DET_SCRIPT_DIR/../common/remap_coco.py --input $RVC_DATA_SRC_DIR/oid/annotations/openimages_challenge_2019_val_bbox.json \
+                            --mapping $RVC_OBJ_DET_SCRIPT_DIR/obj_det_mapping.csv \
+                            --mapping_row oid_boxable_leaf \
+                            --image_root_rel $RVC_DATA_SRC_DIR/oid/val/ \
+                            --void_id 0 \
+                            --reduce_boxable \
+                            --output $RVC_DATA_TRG_DIR/oid_boxable.rvc_val.json
+fi
 
-python3 $RVC_OBJ_DET_SCRIPT_DIR/../common/remap_coco.py --input $RVC_DATA_SRC_DIR/oid/annotations/openimages_challenge_2019_val_bbox.json \
-                        --mapping $RVC_OBJ_DET_SCRIPT_DIR/obj_det_mapping.csv \
-                        --mapping_row oid_boxable_leaf \
-                        --image_root_rel $RVC_DATA_SRC_DIR/oid/val/ \
-                        --void_id 0 \
-                        --reduce_boxable \
-                        --output $RVC_DATA_TRG_DIR/oid_boxable.rvc_val.json
+if [ ! -f "$RVC_DATA_TRG_DIR/oid_boxable.rvc_train.json" ]; then
+    python3 $RVC_OBJ_DET_SCRIPT_DIR/../common/remap_coco.py --input $RVC_DATA_SRC_DIR/oid/annotations/openimages_challenge_2019_train_bbox.json \
+                            --mapping $RVC_OBJ_DET_SCRIPT_DIR/obj_det_mapping.csv \
+                            --mapping_row oid_boxable_leaf \
+                            --image_root_rel "$RVC_DATA_SRC_DIR/oid/train_{file_name[0]}/" \
+                            --void_id 0 \
+                            --reduce_boxable \
+                            --output $RVC_DATA_TRG_DIR/oid_boxable.rvc_train.json
+fi
 
-python3 $RVC_OBJ_DET_SCRIPT_DIR/../common/remap_coco.py --input $RVC_DATA_SRC_DIR/oid/annotations/openimages_challenge_2019_train_bbox.json \
-                        --mapping $RVC_OBJ_DET_SCRIPT_DIR/obj_det_mapping.csv \
-                        --mapping_row oid_boxable_leaf \
-                        --image_root_rel "$RVC_DATA_SRC_DIR/oid/train_{file_name[0]}/" \
-                        --void_id 0 \
-                        --reduce_boxable \
-                        --output $RVC_DATA_TRG_DIR/oid_boxable.rvc_train.json
+if [ ! -f "$RVC_DATA_TRG_DIR/mvd_boxable.rvc_val.json" ]; then
+    python3 $RVC_OBJ_DET_SCRIPT_DIR/../common/remap_coco.py --input $RVC_DATA_SRC_DIR/mvd/validation/panoptic/panoptic_2018.json \
+                            --mapping $RVC_OBJ_DET_SCRIPT_DIR/obj_det_mapping.csv \
+                            --mapping_row mvd_boxable_name \
+                            --image_root_rel $RVC_DATA_SRC_DIR/mvd/validation/images \
+                            --void_id 0 \
+                            --reduce_boxable \
+                            --output $RVC_DATA_TRG_DIR/mvd_boxable.rvc_val.json
+fi
 
-python3 $RVC_OBJ_DET_SCRIPT_DIR/../common/remap_coco.py --input $RVC_DATA_SRC_DIR/mvd/validation/panoptic/panoptic_2018.json \
-                        --mapping $RVC_OBJ_DET_SCRIPT_DIR/obj_det_mapping.csv \
-                        --mapping_row mvd_boxable_name \
-                        --image_root_rel $RVC_DATA_SRC_DIR/mvd/validation/images \
-                        --void_id 0 \
-                        --reduce_boxable \
-                        --output $RVC_DATA_TRG_DIR/mvd_boxable.rvc_val.json
-
-python3 $RVC_OBJ_DET_SCRIPT_DIR/../common/remap_coco.py --input $RVC_DATA_SRC_DIR/mvd/training/panoptic/panoptic_2018.json \
-                        --mapping $RVC_OBJ_DET_SCRIPT_DIR/obj_det_mapping.csv \
-                        --mapping_row mvd_boxable_name \
-                        --image_root_rel $RVC_DATA_SRC_DIR/mvd/training/images  \
-                        --void_id 0 \
-                        --reduce_boxable \
-                        --output $RVC_DATA_TRG_DIR/mvd_boxable.rvc_train.json
+if [ ! -f "$RVC_DATA_TRG_DIR/mvd_boxable.rvc_train.json" ]; then
+    python3 $RVC_OBJ_DET_SCRIPT_DIR/../common/remap_coco.py --input $RVC_DATA_SRC_DIR/mvd/training/panoptic/panoptic_2018.json \
+                            --mapping $RVC_OBJ_DET_SCRIPT_DIR/obj_det_mapping.csv \
+                            --mapping_row mvd_boxable_name \
+                            --image_root_rel $RVC_DATA_SRC_DIR/mvd/training/images  \
+                            --void_id 0 \
+                            --reduce_boxable \
+                            --output $RVC_DATA_TRG_DIR/mvd_boxable.rvc_train.json
+fi
 						
 pushd $RVC_DATA_TRG_DIR
 python3 $RVC_OBJ_DET_SCRIPT_DIR/../common/join_coco.py --join "coco_boxable.rvc_val.json;obj365_boxable.rvc_val.json;oid_boxable.rvc_val.json;mvd_boxable.rvc_val.json" \
