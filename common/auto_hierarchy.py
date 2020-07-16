d851b49c122ff17f2c5e777d7fb080a19d6429bd
Oliver Zendel <oliver.zendel@ait.ac.at>
2020-07-16 21:49:42 +0200

    -preparing automatic hierarchy generation

--- common/auto_hierarchy.py
+++ common/auto_hierarchy.py
@@ -9,6 +9,16 @@ from automap import get_wordnet_gloss, wikidata_from_qid, get_wikidata
 retry_time_sleep_s = 1.0
 max_retries_wikidata = 3
 
+def get_parents(vals):
+    if 'parent_name' in vals:
+        return
+    if 'wikidata_qid' in vals and len(vals['wikidata_qid']) > 0 and not 'parents_qid' in vals:
+        add0 = wikidata_from_qid(vals['wikidata_qid'])
+        vals['parents_qid'] = add0.get('parents_qid',[])
+    if 'wordnet_pwn30' in vals and len(vals['wordnet_pwn30']) > 0  and not 'parents_pwn30' in vals:
+        add0 = get_wordnet_gloss(vals['wordnet_pwn30'])
+        vals['parents_pwn30'] = add0.get('parents_pwn30',[])
+
 def main(argv=sys.argv[1:]):
     parser = argparse.ArgumentParser()
     parser.add_argument('--input', type=str, default="joint_mapping.json",
@@ -22,52 +32,51 @@ def main(argv=sys.argv[1:]):
 
     #automatically adds parent candidates 
     for key, vals in joined_label_space.items():
-        if 'parent_name' in vals:
-            continue
-        if 'wikidata_qid' in vals and len(vals['wikidata_qid']) > 0 and not 'parents_qid' in vals:
-            add0 = wikidata_from_qid(vals['wikidata_qid'])
-            if 'parents_qid' in add0:
-                vals['parents_qid'] = add0['parents_qid']
-                
-        if 'wordnet_pwn30' in vals and len(vals['wordnet_pwn30']) > 0  and not 'parents_pwn30' in vals:
-            add0 = get_wordnet_gloss(vals['wordnet_pwn30'])
-            if 'parents_pwn30' in add0:
-                vals['parents_pwn30'] = add0['parents_pwn30']
-
+        get_parents(vals)
+        
     #find direct parents
     link_pwn30 = {vals['wordnet_pwn30']:key  for key,vals in joined_label_space.items() if 'wordnet_pwn30' in vals}
     link_qid = {vals['wikidata_qid']:key  for  key,vals in joined_label_space.items() if 'wikidata_qid' in vals}
-    tmp_keys = {}
-    for key, vals in joined_label_space.items():
-        if 'parent_name' in vals:
-            continue
-        found_link0 = [link_pwn30[p] for p in vals.get('parents_pwn30',[]) if p in link_pwn30]
-        found_link1 = [link_qid[p] for p in vals.get('wikidata_qid',[]) if p in link_qid]
-        found_link0 = set(found_link0+found_link1)
-        if len(found_link0) == 1:
-            vals['parent_name'] = found_link0[0]
-        elif len(found_link0) > 1:
-            print("Warning: Multiple parents found for "+key+':', found_link0)
-        else:
-            #add temporary jump nodes
-            for p in vals.get('wikidata_qid',[]):
-                addtmp = wikidata_from_qid(p)
-                if "wordnet_pwn30" in addtmp and addtmp["wordnet_pwn30"] in link_pwn30:
-                    print("Warning: rebending parent of "+key+" to "+ link_pwn30[addtmp["wordnet_pwn30"]])
-                    vals['parent_name'] = link_pwn30[addtmp["wordnet_pwn30"]]
-                    break
-                if "wordnet_name" in addtmp:
-                    key = "_tmp_"+addtmp["wordnet_name"]
-                    tmp_keys[key] = addtmp
+    
+    for retry in range(2): #first pass creates direct connections, second pass those over one jump
+        tmp_keys = {}
+        for key, vals in joined_label_space.items():
             if 'parent_name' in vals:
                 continue
-            for p in vals.get('parents_pwn30',[]):
-                addtmp = get_wordnet_gloss(p)
-                if "wordnet_name" in addtmp:
-                    key = "_tmp_"+addtmp["wordnet_name"]
-                    tmp_keys.setdefault(key, {}).update(addtmp)
-              
-    joined_label_space.update(tmp_keys) 
+            found_link0 = [link_pwn30[p] for p in vals.get('parents_pwn30',[]) if p in link_pwn30]
+            found_link1 = [link_qid[p] for p in vals.get('wikidata_qid',[]) if p in link_qid]
+            found_link0 = set(found_link0+found_link1)
+            found_link0_clean = set([p for p in list(found_link0) if p.find("_tmp") < 0])
+            if len(found_link0_clean) > 0:
+                found_link0 = found_link0_clean #skip tmp entries for regular ones
+            if len(found_link0) == 1:
+                key_parent = list(found_link0)[0]
+                vals['parent_name'] = key_parent
+            elif len(found_link0) > 1:
+                print("Warning: Multiple parents found for "+key+':', found_link0)
+            else:
+                #add temporary jump nodes
+                for p in vals.get('wikidata_qid',[]):
+                    addtmp = wikidata_from_qid(p)
+                    if "wordnet_pwn30" in addtmp and addtmp["wordnet_pwn30"] in link_pwn30:
+                        print("Warning: rebending parent of "+key+" to "+ link_pwn30[addtmp["wordnet_pwn30"]])
+                        vals['parent_name'] = link_pwn30[addtmp["wordnet_pwn30"]]
+                        break
+                    if "wordnet_name" in addtmp:
+                        key = "_tmp_"+addtmp["wordnet_name"]
+                        get_parents(addtmp)
+                        tmp_keys.setdefault(key, {}).update(addtmp)
+                        link_qid[p] = key
+                if 'parent_name' in vals:
+                    continue
+                for p in vals.get('parents_pwn30',[]):
+                    addtmp = get_wordnet_gloss(p)
+                    if "wordnet_name" in addtmp:
+                        key = "_tmp_"+addtmp["wordnet_name"]
+                        get_parents(addtmp)
+                        tmp_keys.setdefault(key, {}).update(addtmp)
+                        link_pwn30[p] = key
+        joined_label_space.update(tmp_keys) 
 
     #print("Found mappings for %i entries and %i have qids of %i" % (cnt_both , cnt_qid, len(joined_label_space)))
     with open(args.output, 'w', newline='\n') as ofile:
