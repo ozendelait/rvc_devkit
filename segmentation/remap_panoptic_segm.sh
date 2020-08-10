a7105422383b559bf28402042de76cff5a4f90d8
Oliver Zendel <oliver.zendel@ait.ac.at>
2020-08-11 00:10:51 +0200

    -fixed used python version

--- segmentation/remap_panoptic_segm.sh
+++ segmentation/remap_panoptic_segm.sh
@@ -19,7 +19,7 @@ else
 fi
 
 if [ ! -f "$RVC_DATA_TRG_DIR/coco_pano.rvc_val.json" ]; then
-  python $RVC_OBJ_DET_SCRIPT_DIR/../common/remap_coco.py --input $RVC_DATA_SRC_DIR/coco/annotations/panoptic_val2017.json \
+  python3 $RVC_OBJ_DET_SCRIPT_DIR/../common/remap_coco.py --input $RVC_DATA_SRC_DIR/coco/annotations/panoptic_val2017.json \
                         --mapping $RVC_OBJ_DET_SCRIPT_DIR/pano_mapping.csv \
                         --mapping_row coco_pano_name \
                         --image_root_rel $RVC_DATA_SRC_DIR/coco/images \
@@ -29,7 +29,7 @@ if [ ! -f "$RVC_DATA_TRG_DIR/coco_pano.rvc_val.json" ]; then
 fi
 
 if [ ! -f "$RVC_DATA_TRG_DIR/coco_pano.rvc_train.json" ]; then
-  python $RVC_OBJ_DET_SCRIPT_DIR/../common/remap_coco.py --input $RVC_DATA_SRC_DIR/coco/annotations/panoptic_train2017.json \
+  python3 $RVC_OBJ_DET_SCRIPT_DIR/../common/remap_coco.py --input $RVC_DATA_SRC_DIR/coco/annotations/panoptic_train2017.json \
                         --mapping $RVC_OBJ_DET_SCRIPT_DIR/pano_mapping.csv \
                         --mapping_row coco_pano_name \
                         --image_root_rel $RVC_DATA_SRC_DIR/coco/images \
@@ -39,7 +39,7 @@ if [ ! -f "$RVC_DATA_TRG_DIR/coco_pano.rvc_train.json" ]; then
 fi
 
 if [ ! -f "$RVC_DATA_TRG_DIR/mvd_pano.rvc_val.json" ]; then
-  python $RVC_OBJ_DET_SCRIPT_DIR/../common/remap_coco.py --input $RVC_DATA_SRC_DIR/mvd/validation/panoptic/panoptic_2018.json \
+  python3 $RVC_OBJ_DET_SCRIPT_DIR/../common/remap_coco.py --input $RVC_DATA_SRC_DIR/mvd/validation/panoptic/panoptic_2018.json \
                         --mapping $RVC_OBJ_DET_SCRIPT_DIR/pano_mapping.csv \
                         --mapping_row mvd_pano_name \
                         --image_root_rel $RVC_DATA_SRC_DIR/mvd/validation/images \
@@ -49,7 +49,7 @@ if [ ! -f "$RVC_DATA_TRG_DIR/mvd_pano.rvc_val.json" ]; then
 fi
 
 if [ ! -f "$RVC_DATA_TRG_DIR/mvd_pano.rvc_train.json" ]; then
-  python $RVC_OBJ_DET_SCRIPT_DIR/../common/remap_coco.py --input $RVC_DATA_SRC_DIR/mvd/training/panoptic/panoptic_2018.json  \
+  python3 $RVC_OBJ_DET_SCRIPT_DIR/../common/remap_coco.py --input $RVC_DATA_SRC_DIR/mvd/training/panoptic/panoptic_2018.json  \
                         --mapping $RVC_OBJ_DET_SCRIPT_DIR/pano_mapping.csv \
                         --mapping_row mvd_pano_name \
                         --image_root_rel $RVC_DATA_SRC_DIR/mvd/training/images  \
@@ -59,7 +59,7 @@ if [ ! -f "$RVC_DATA_TRG_DIR/mvd_pano.rvc_train.json" ]; then
 fi
 
 if [ ! -f "$RVC_DATA_TRG_DIR/viper_pano.rvc_train.json" ]; then
-  python $RVC_OBJ_DET_SCRIPT_DIR/../common/remap_coco.py --input $RVC_DATA_SRC_DIR/viper/train/pano.json \
+  python3 $RVC_OBJ_DET_SCRIPT_DIR/../common/remap_coco.py --input $RVC_DATA_SRC_DIR/viper/train/pano.json \
                         --mapping $RVC_OBJ_DET_SCRIPT_DIR/pano_mapping.csv \
                         --mapping_row viper_pano_name \
                         --image_root_rel $RVC_DATA_SRC_DIR/viper/train/img/{file_name[0]}{file_name[1]}{file_name[2]} \
@@ -69,7 +69,7 @@ if [ ! -f "$RVC_DATA_TRG_DIR/viper_pano.rvc_train.json" ]; then
 fi
 
 if [ ! -f "$RVC_DATA_TRG_DIR/viper_pano.rvc_val.json" ]; then
-  python $RVC_OBJ_DET_SCRIPT_DIR/../common/remap_coco.py --input $RVC_DATA_SRC_DIR/viper/val/pano.json \
+  python3 $RVC_OBJ_DET_SCRIPT_DIR/../common/remap_coco.py --input $RVC_DATA_SRC_DIR/viper/val/pano.json \
                         --mapping $RVC_OBJ_DET_SCRIPT_DIR/pano_mapping.csv \
                         --mapping_row viper_pano_name \
                         --image_root_rel $RVC_DATA_SRC_DIR/viper/val/img/{file_name[0]}{file_name[1]}{file_name[2]} \
@@ -80,11 +80,11 @@ fi
 
 if [ ! -f "$RVC_DATA_SRC_DIR/wilddash/panoptic_0.json" ]; then
 #Creates random split for train/val (currently no specific split supplied)
-  python $RVC_OBJ_DET_SCRIPT_DIR/../common/rvc_split_coco.py --input $RVC_DATA_SRC_DIR/wilddash/panoptic.json --split "80;20"
+  python3 $RVC_OBJ_DET_SCRIPT_DIR/../common/rvc_split_coco.py --input $RVC_DATA_SRC_DIR/wilddash/panoptic.json --split "80;20"
 fi
 
 if [ ! -f "$RVC_DATA_SRC_DIR/wd2_pano.rvc_train.json" ]; then
-  python $RVC_OBJ_DET_SCRIPT_DIR/../common/remap_coco.py --input $RVC_DATA_SRC_DIR/wilddash/panoptic_0.json \
+  python3 $RVC_OBJ_DET_SCRIPT_DIR/../common/remap_coco.py --input $RVC_DATA_SRC_DIR/wilddash/panoptic_0.json \
                         --mapping $RVC_OBJ_DET_SCRIPT_DIR/pano_mapping.csv \
                         --mapping_row wilddash_pano_name \
                         --image_root_rel $RVC_DATA_SRC_DIR/wilddash/images \
@@ -94,7 +94,7 @@ if [ ! -f "$RVC_DATA_SRC_DIR/wd2_pano.rvc_train.json" ]; then
 fi
 
 if [ ! -f "$RVC_DATA_SRC_DIR/wd2_pano.rvc_val.json" ]; then
-  python $RVC_OBJ_DET_SCRIPT_DIR/../common/remap_coco.py --input $RVC_DATA_SRC_DIR/wilddash/panoptic_1.json \
+  python3 $RVC_OBJ_DET_SCRIPT_DIR/../common/remap_coco.py --input $RVC_DATA_SRC_DIR/wilddash/panoptic_1.json \
                         --mapping $RVC_OBJ_DET_SCRIPT_DIR/pano_mapping.csv \
                         --mapping_row wilddash_pano_name \
                         --image_root_rel $RVC_DATA_SRC_DIR/wilddash/images \
@@ -104,7 +104,7 @@ if [ ! -f "$RVC_DATA_SRC_DIR/wd2_pano.rvc_val.json" ]; then
 fi
 
 if [ ! -f "$RVC_DATA_SRC_DIR/cs_pano.rvc_train.json" ]; then
-  python $RVC_OBJ_DET_SCRIPT_DIR/../common/remap_coco.py --input $RVC_DATA_SRC_DIR/cityscapes/panoptic/cityscapes_panoptic_val.json \
+  python3 $RVC_OBJ_DET_SCRIPT_DIR/../common/remap_coco.py --input $RVC_DATA_SRC_DIR/cityscapes/panoptic/cityscapes_panoptic_val.json \
                         --mapping $RVC_OBJ_DET_SCRIPT_DIR/pano_mapping.csv \
                         --mapping_row cityscapes_pano_name \
                         --image_root_rel $RVC_DATA_SRC_DIR/cityscapes/leftImg8bit_sequence/val/{frame_name_su[0]}/ \
@@ -114,7 +114,7 @@ if [ ! -f "$RVC_DATA_SRC_DIR/cs_pano.rvc_train.json" ]; then
 fi
 
 if [ ! -f "$RVC_DATA_SRC_DIR/cs_pano.rvc_val.json" ]; then
-python $RVC_OBJ_DET_SCRIPT_DIR/../common/remap_coco.py --input $RVC_DATA_SRC_DIR/cityscapes/panoptic/cityscapes_panoptic_train.json \
+  python3 $RVC_OBJ_DET_SCRIPT_DIR/../common/remap_coco.py --input $RVC_DATA_SRC_DIR/cityscapes/panoptic/cityscapes_panoptic_train.json \
                         --mapping $RVC_OBJ_DET_SCRIPT_DIR/pano_mapping.csv \
                         --mapping_row cityscapes_pano_name \
                         --image_root_rel $RVC_DATA_SRC_DIR/cityscapes/leftImg8bit_sequence/train/{frame_name_su[0]}/ \
@@ -125,9 +125,9 @@ fi
 
                         
 pushd $RVC_DATA_TRG_DIR
-python $RVC_OBJ_DET_SCRIPT_DIR/../common/join_coco.py --join "coco_pano.rvc_val.json;wd2_pano.rvc_val.json;viper_pano.rvc_val.json;cs_pano.rvc_val.json;mvd_pano.rvc_val.json" \
+python3 $RVC_OBJ_DET_SCRIPT_DIR/../common/join_coco.py --join "coco_pano.rvc_val.json;wd2_pano.rvc_val.json;viper_pano.rvc_val.json;cs_pano.rvc_val.json;mvd_pano.rvc_val.json" \
        --output joined_val_pano.json
-python $RVC_OBJ_DET_SCRIPT_DIR/../common/join_coco.py --join "coco_pano.rvc_train.json;wd2_pano.rvc_train.json;viper_pano.rvc_train.json;cs_pano.rvc_train.json;mvd_pano.rvc_train.json" \
+python3 $RVC_OBJ_DET_SCRIPT_DIR/../common/join_coco.py --join "coco_pano.rvc_train.json;wd2_pano.rvc_train.json;viper_pano.rvc_train.json;cs_pano.rvc_train.json;mvd_pano.rvc_train.json" \
        --output joined_train_pano.json
 popd
 
